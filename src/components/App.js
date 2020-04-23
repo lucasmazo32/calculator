66f74dc4fc6b4fdcc8fae8b3e70a24902a6c8d26
Lucas Mazo <lucasmazo32@gmail.com>
2020-04-23 17:08:20 -0500

    creation of all the components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Big from 'big.js';
 
-function name(params) {
-  
-}
-
-export default function App() {
+function Display(props) {
   return (
-    <div>Hello World</div>
+    <span className="display">{props.result}</span>
   );
 }
+
+Display.defaultProps = {
+  result: '0',
+};
+
+Display.propTypes = {
+  result: PropTypes.string,
+};
+
+function Button(props) {
+  return <button>{props.name}</button>;
+}
+
+Button.propTypes = {
+  name: PropTypes.string,
+};
+
+function ButtonPanel() {
+  return (
+    <div>
+      <div>
+        <Button name="AC" />
+        <Button name="+/-" />
+        <Button name="%" />
+        <Button name="÷" />
+      </div>
+      <div>
+        <Button name="7" />
+        <Button name="8" />
+        <Button name="9" />
+        <Button name="X" />
+      </div>
+      <div>
+        <Button name="4" />
+        <Button name="5" />
+        <Button name="6" />
+        <Button name="-" />
+      </div>
+      <div>
+        <Button name="1" />
+        <Button name="2" />
+        <Button name="3" />
+        <Button name="+" />
+      </div>
+      <div>
+        <Button name="0" />
+        <Button name="." />
+        <Button name="=" />
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="container">
+      <Display />
+      <ButtonPanel />
+    </div>
+  );
+}
+
+export default App;
