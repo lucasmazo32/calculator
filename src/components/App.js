c5c696df6865aa66de02891cd2a30c814f887d24
Lucas Mazo <lucasmazo32@gmail.com>
2020-04-23 14:20:06 -0500

    creating componets for the calculator

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Big from 'big.js';
 
+function name(params) {
+  
+}
+
 export default function App() {
   return (
     <div>Hello World</div>
