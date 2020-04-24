82961dda0644918b87222c58bb87d76e8a54b93f
Lucas Mazo <lucasmazo32@gmail.com>
2020-04-24 10:35:48 -0500

    Finishing the styling

--- src/components/App.js
+++ src/components/App.js
@@ -28,31 +28,31 @@ Button.propTypes = {
 function ButtonPanel() {
   return (
     <div>
-      <div>
+      <div className="btn-container">
         <Button name="AC" />
         <Button name="+/-" />
         <Button name="%" />
         <Button name="÷" />
       </div>
-      <div>
+      <div className="btn-container">
         <Button name="7" />
         <Button name="8" />
         <Button name="9" />
         <Button name="X" />
       </div>
-      <div>
+      <div className="btn-container">
         <Button name="4" />
         <Button name="5" />
         <Button name="6" />
         <Button name="-" />
       </div>
-      <div>
+      <div className="btn-container">
         <Button name="1" />
         <Button name="2" />
         <Button name="3" />
         <Button name="+" />
       </div>
-      <div>
+      <div className="btn-container t-btn">
         <Button name="0" />
         <Button name="." />
         <Button name="=" />
