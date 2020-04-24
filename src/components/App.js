e48b45ed7108fe83dd9756d5f07083bce622bb7b
Lucas Mazo <lucasmazo32@gmail.com>
2020-04-24 16:12:27 -0500

    added function for click event

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-// import calculate from '../logic/calculate';
+import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div className="container">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick(buttonName) {
+    const { total, next } = this.state;
+    const result = calculate(total, next, buttonName);
+    this.setState({
+      total: next,
+      operation: buttonName,
+      next: result,
+    });
+  }
+
+  render() {
+    const { next } = this.state;
+    return (
+      <div className="container">
+        <Display result={next || '0'} />
+        <ButtonPanel onClick={this.handleClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
