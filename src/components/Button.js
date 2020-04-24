c58459b52be50fce77fe2dba7a0c19859574131f
Lucas Mazo <lucasmazo32@gmail.com>
2020-04-24 11:13:10 -0500

    made inline styles for the buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+function styleObj(wide, color) {
+  if (wide) {
+    return {
+      backgroundColor: color,
+      flexBasis: '50%',
+    };
+  }
+  return {
+    backgroundColor: color,
+    flexBasis: '25%',
+  };
+}
+
 export default function Button(props) {
   const { name } = props;
-  return <button type="button">{name}</button>;
+  const { color } = props;
+  const { wide } = props;
+  const btnStyle = styleObj(wide, color);
+  return <button type="button" style={btnStyle}>{name}</button>;
 }
+
+Button.defaultProps = {
+  color: '#f5913e',
+  wide: false,
+};
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
 };
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -4,33 +4,33 @@ import Button from './Button';
 export default function ButtonPanel() {
   return (
     <div>
-      <div>
-        <Button name="AC" />
-        <Button name="+/-" />
-        <Button name="%" />
+      <div className="btn-container">
+        <Button name="AC" color="#e1e1e1" />
+        <Button name="+/-" color="#e1e1e1" />
+        <Button name="%" color="#e1e1e1" />
         <Button name="÷" />
       </div>
-      <div>
-        <Button name="7" />
-        <Button name="8" />
-        <Button name="9" />
+      <div className="btn-container">
+        <Button name="7" color="#e1e1e1" />
+        <Button name="8" color="#e1e1e1" />
+        <Button name="9" color="#e1e1e1" />
         <Button name="X" />
       </div>
-      <div>
-        <Button name="4" />
-        <Button name="5" />
-        <Button name="6" />
+      <div className="btn-container">
+        <Button name="4" color="#e1e1e1" />
+        <Button name="5" color="#e1e1e1" />
+        <Button name="6" color="#e1e1e1" />
         <Button name="-" />
       </div>
-      <div>
-        <Button name="1" />
-        <Button name="2" />
-        <Button name="3" />
+      <div className="btn-container">
+        <Button name="1" color="#e1e1e1" />
+        <Button name="2" color="#e1e1e1" />
+        <Button name="3" color="#e1e1e1" />
         <Button name="+" />
       </div>
-      <div>
-        <Button name="0" />
-        <Button name="." />
+      <div className="btn-container">
+        <Button name="0" color="#e1e1e1" wide />
+        <Button name="." color="#e1e1e1" />
         <Button name="=" />
       </div>
     </div>
