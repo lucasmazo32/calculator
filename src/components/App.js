f2ec9a1ceed69809912b0e7596b939ccfa93d1da
Lucas Mazo <lucasmazo32@gmail.com>
2020-04-24 21:21:20 -0500

    solving branches merged

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,15 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-import calculate from '../logic/calculate';
+// import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
-
-  handleClick(buttonName) {
-    const { total, next } = this.state;
-    const result = calculate(total, next, buttonName);
-    this.setState({
-      total: next,
-      operation: buttonName,
-      next: result,
-    });
-  }
-
-  render() {
-    const { next } = this.state;
-    return (
-      <div className="container">
-        <Display result={next || '0'} />
-        <ButtonPanel onClick={this.handleClick} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="container">
+      <Display />
+      <ButtonPanel />
+    </div>
+  );
 }
 
 export default App;
