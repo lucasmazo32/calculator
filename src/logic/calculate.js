0b68e787b60dc04b2b3c6a73537ceb3ceef9061e
Lucas Mazo <lucasmazo32@gmail.com>
2020-04-26 11:55:56 -0500

    Worked with negatives and the possibility of starting with a negative sign

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -6,10 +6,14 @@ import operate from './operate';
 export default function calculate(calculation, btnName) {
   const { next } = calculation;
   const opRegex = /[+\-÷X]/;
-  const plusMinus = /^\d+\.*\d*[+\-÷X]/;
+  const lengthRegex = /[+\-÷X]/g;
+  const opRegexFirst = /[+÷X]/;
+  const operatorRegex = /^-\d+\.*\d*[+\-÷X]/;
+  const startingNegative = /^-\d+\.*\d*$/;
+  const plusMinus = /^-*\d+\.*\d*[+\-÷X]/;
   const eqReg = /[+\-÷X]\.*\d+/;
   let newTotal = null;
-  if (!plusMinus.test(next)) {
+  if (!plusMinus.test(next) && next !== '-') {
     newTotal = Big(next);
   }
   let newNext;
@@ -46,7 +50,12 @@ export default function calculate(calculation, btnName) {
       break;
     case '=':
       if (next.match(eqReg)) {
-        const pos = next.match(eqReg).index;
+        let pos;
+        if (next.match(lengthRegex).length === 2) {
+          pos = next.slice(1).match(new RegExp(`\\${next.match(lengthRegex)[1]}`)).index + 1;
+        } else {
+          pos = next.match(eqReg).index;
+        }
         newNext = operate(next.slice(0, pos), next.slice(pos + 1), next.slice(pos, pos + 1));
       } else {
         newNext = next;
@@ -54,12 +63,14 @@ export default function calculate(calculation, btnName) {
       break;
     default:
       if (next === '0') {
-        if (!opRegex.test(btnName)) {
+        if (!opRegexFirst.test(btnName)) {
           newNext = btnName;
         } else {
           newNext = '0';
         }
-      } else if (opRegex.test(next)) {
+      } else if (startingNegative.test(next)) {
+        newNext = `${next}${btnName}`;
+      } else if (operatorRegex.test(next)) {
         if (opRegex.test(btnName)) {
           newNext = next;
         } else {
